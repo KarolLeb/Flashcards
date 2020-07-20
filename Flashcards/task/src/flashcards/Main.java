707942b2a75479ebea7d509043ae03ef8c41da15
Karol Lebiodzik <karol.lebiodzik@gmail.com>
2020-07-20 22:43:52 +0200

    Changed to LinkedHashMap Finally adjusted annoying output ^^ Completed stage 4

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -1,20 +1,26 @@
 package flashcards;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Environment env = new Environment();
+        System.out.println("Input the number of cards:");
         env.setNumberOfCards(Integer.parseInt(scanner.nextLine()));
-        for (int i = 0; i < env.getNumberOfCards(); i++) {
-            env.cards.put(scanner.nextLine(), scanner.nextLine());
+        for (int i = 1; i <= env.getNumberOfCards(); i++) {
+            System.out.println("The card #" + i + ":");
+            env.addNewCard(i);
         }
-        for (HashMap.Entry<String, String> c : env.cards.entrySet()) {
+        for (Map.Entry<String, String> c : env.cards.entrySet()) {
             System.out.println("Print the definition of \"" + c.getKey() + "\"");
-            if (c.getValue().equals(scanner.nextLine())) {
-                System.out.println("Correct answer");
+            String answer = scanner.nextLine();
+            if (c.getValue().equals(answer)) {
+                System.out.println("Correct answer.");
+            } else if (env.cards.containsValue(answer)) {
+                System.out.println("Wrong answer. The correct one is \"" + c.getValue() + "\", you've just written the definition of \"" + env.revCards.get(answer) + "\".");
             } else {
                 System.out.println("Wrong answer. The correct one is \"" + c.getValue() + "\".");
             }
@@ -22,12 +28,38 @@ public class Main {
     }
 
     private static class Environment {
-        HashMap<String, String> cards;
+        LinkedHashMap<String, String> cards;
+        LinkedHashMap<String, String> revCards;
         private int numberOfCards;
 
         public Environment() {
             this.numberOfCards = 0;
-            this.cards = new HashMap<>();
+            this.cards = new LinkedHashMap<>();
+            this.revCards = new LinkedHashMap<>();
+        }
+
+        public void addNewCard(int i) {
+            Scanner scanner = new Scanner(System.in);
+            String key = scanner.nextLine();
+            if (cards.containsKey(key)) {
+                System.out.println("The card \"" + key + "\" already exists. Try again:");
+                addNewCard(i);
+            } else {
+                System.out.println("The definition of the card #" + i + ":");
+                addDefinition(key);
+            }
+        }
+
+        public void addDefinition(String key) {
+            Scanner scanner = new Scanner(System.in);
+            String value = scanner.nextLine();
+            if (cards.containsValue(value)) {
+                System.out.println("The definition \"" + value + "\" already exists. Try again:");
+                addDefinition(key);
+            } else {
+                cards.put(key, value);
+                revCards.put(value, key);
+            }
         }
 
         public int getNumberOfCards() {
